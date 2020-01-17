6894778bcd140bc321ebe408ee73b5ab222d866f
Pusendra <cpusendra@gmail.com>
2020-01-18 01:23:59 +0545

    jpgtopngconverter

---
+++ jtop.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from PIL import Image
+
+#grab the first and second argument
+image_folder =sys.argv[1]
+output_folder = sys.argv[2]
+
+
+#check if new exist if not create it
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+
+
+#loop through folder Pokedex
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    '''print(clean_name)
+    ('bulbasaur', '.jpg')
+    ('charmander', '.jpg')
+    ('pikachu', '.jpg')
+    ('squirtle', '.jpg')
+
+
+    
+    '''
+    #convert images to PNG,#save new to folder
+    img.save(f'{output_folder}{clean_name}.png','png')
+    print('all done!')
+
+#save new to folder
